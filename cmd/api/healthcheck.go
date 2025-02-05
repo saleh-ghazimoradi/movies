26432ae8f06c09ac3166385f8556b860915a409e
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-02-05 09:06:35 +0330

    add some helper functions and movie model to the project

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, data, nil); err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
 }
---
+++ cmd/api/movies.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/saleh-ghazimoradi/movies/internal/data"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Year:      2021,
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	if err = app.writeJSON(w, http.StatusOK, movie, nil); err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
+}
